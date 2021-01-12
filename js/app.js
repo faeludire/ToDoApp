4e734691f725e14a2a5e4e0c866e46540df945cf
Fiyinfolu Eludire <17176053+faeludire@users.noreply.github.com>
2021-01-12 22:10:18 +0000

    Add TodoListCard

--- js/app.js
+++ js/app.js
@@ -3,12 +3,44 @@ function App() {
     return (
         <Container>
             <Row>
-                <Col>
-                    <h1>Hello!</h1>
+                <Col md={{offset: 3, span: 6}}>
+                    <TodoListCard />
                 </Col>
             </Row>
         </Container>
     );
 }
 
+function TodoListCard() {
+    const [items, setItems] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch('./items')
+            .then(r => r.json())
+            .then(setItems)
+    }, []);
+
+    const onNewItem = React.useCallback(
+        newItem => {
+            setItems([...items, newItem]);
+        }, [items],
+    );
+
+    const onItemRemoval = React.useCallback(
+        item => {
+            const index = items.findIndex(i => i.id === item.id);
+            setItems([...items.slice(0, index), ...items.slice(index + 1)]);
+        }, [items],
+    );
+
+    if (items === null ) return 'Loading...';
+
+    return (
+        <React.Fragment>
+            {items.length === 0 && (
+                <p className="text-center">You have no todo items yet! Add one above!</p>
+            )}
+        </React.Fragment>
+    )
+}
 ReactDOM.render(<App />, document.getElementById('root'));
